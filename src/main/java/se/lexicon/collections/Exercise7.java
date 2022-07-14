373058b2fe3882fa9ffb64c665ae982244fceea0
Jonas Eira <jonas.eira@gmail.com>
2022-07-14 09:39:17 +0200

    Seven exercises

--- src/main/java/se/lexicon/collections/Exercise7.java
+++ src/main/java/se/lexicon/collections/Exercise7.java
@@ -1,6 +1,20 @@
 package se.lexicon.collections;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+
 public class Exercise7 {
     public static void run() {
-    }
+        HashSet<String> names = new HashSet<String>();
+        names.add("Name1");
+        names.add("Name2");
+        names.add("Name4");
+        names.add("Name3");
+        names.add("Name5");
+        names.add("Name6");
+        ArrayList<String> namesList = new ArrayList<String>(names);
+        Collections.sort(namesList);
+        System.out.println(namesList);
+     }
 }
